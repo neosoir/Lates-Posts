bc358c8fefe8d23df2930860dc202a1a9c28dffb
Neo Soir <leonard@neosoir.com>
2022-12-07 14:09:16 -0600

    [add] fist complete plugin

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '91092d418659f4142ac7');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'c23731b1546af624b20d');
